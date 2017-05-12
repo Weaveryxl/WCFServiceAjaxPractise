4cc359448e6c3bb71ae096352ca2cfa9d9228469
Weaveryxl <weaveryxl549@gmail.com>
2017-05-12 13:13:14 -0400

    WCFServiceAjaxPractise

---
+++ User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WCFServiceAjaxPractise
+{
+    public class User
+    {
+        Dictionary<int, string> users = null;
+        public User()
+        {
+            users = new Dictionary<int, string>();
+            users.Add(1, "Pranay");
+            users.Add(2, "Krunal");
+            users.Add(3, "Aditya");
+            users.Add(4, "Samir");
+        }
+
+        public string[] GetUser(int Id)
+        {
+            var user = users.Where(u => u.Key == Id).Select(u => u.Value);
+            return user.ToArray<string>();
+        }
+    }
+}
---
+++ WCFAjaxService.svc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Http.Cors;
+
+namespace WCFServiceAjaxPractise
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    //[EnableCors(origins: "http://localhost:62143", headers: "*", methods: "*")]
+    public class WCFAjaxService : IWCFAjaxService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public string[] GetUser(string Id)
+        {
+            return new User().GetUser(Convert.ToInt32(Id));
+        }
+    }
+}
